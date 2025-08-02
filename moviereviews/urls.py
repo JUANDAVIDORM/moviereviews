3a383c036bfeac498b4d3887534b2d7528407dc6
David Ortiz <JUANDAVIDRM>
2025-08-01 19:20:54 -0500

    busqueda de peliculas

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('', movieViews.home),           # <- Ruta principal
     path('about/', movieViews.about),    # <- Ruta de la página About
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
